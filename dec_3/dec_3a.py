5f926ddf3c85ecf38fe78c3076d8c911f232eff0
Richard <richard.a.mck@gmail.com>
2023-12-03 13:05:14 -0800

    feat: move reusable code into shared module

--- dec_3/dec_3a.py
+++ dec_3/dec_3a.py
@@ -20,5 +20,9 @@ In this schematic, two numbers are not part numbers because they are not adjacen
 (middle right). Every other number is adjacent to a symbol and so is a part number; their sum is 4361.
 """
 
+from common_functions import load_input
+
 if __name__ == "__main__":
+    data = load_input("example_a.txt")
+    print(data)
     pass
