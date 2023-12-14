565586fc234faa590d6d678a4df72ab594ee9bfc
Richard McKenzie <richard@openoceanrobotics.com>
2023-12-14 12:29:31 -0800

    fix: ensure initial data loading doens't modify rows and only returns rows with data

--- common_functions.py
+++ common_functions.py
@@ -6,5 +6,5 @@ Reusable functions for loading input and so on
 def load_input(filename: str) -> list:
     """Given a filename load it and return its contents as a list, with each item representing a line"""
     with open(filename, "r") as f:
-        contents = f.read().split()
+        contents = f.read().split("\n")[:-1]
     return contents
