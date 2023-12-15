e7a2caf1f44092b265b8633f79edfa0948a10aa7
Richard McKenzie <richard@openoceanrobotics.com>
2023-12-15 14:34:32 -0800

    docs: add part two documentation and improve class legibility with repr method

--- dec_5/dec_5a.py
+++ dec_5/dec_5a.py
@@ -104,6 +104,24 @@ number. In this example, the corresponding types are:
 So, the lowest location number in this example is 35.
 
 What is the lowest location number that corresponds to any of the initial seed numbers?
+
+Part 2:
+The values on the initial seeds: line come in pairs. Within each pair, the first value is the start of the range and
+the second value is the length of the range. So, in the first line of the example above:
+
+seeds: 79 14 55 13
+
+This line describes two ranges of seed numbers to be planted in the garden. The first range starts with seed number 79
+and contains 14 values: 79, 80, ..., 91, 92. The second range starts with seed number 55 and contains 13 values:
+55, 56, ..., 66, 67.
+
+Now, rather than considering four seed numbers, you need to consider a total of 27 seed numbers.
+
+In the above example, the lowest location number can be obtained from seed number 82, which corresponds to soil 84,
+fertilizer 84, water 84, light 77, temperature 45, humidity 46, and location 46. So, the lowest location number is 46.
+
+Consider all of the initial seed numbers listed in the ranges on the first line of the almanac. What is the lowest
+location number that corresponds to any of the initial seed numbers?
 """
 
 from common_functions import load_input
@@ -114,11 +132,19 @@ class MapRanges(object):
         self.destination = range(destination, destination + range_length)
         self.source = range(source, source + range_length)
 
+    def __repr__(self):
+        class_name = type(self).__name__
+        return f"{class_name}(source={self.source!r}, destination={self.destination!r})"
+
 
 class AlmanacMap(object):
     def __init__(self, name: str, entries: list[str]):
         self.name = name
         self.entries = self.process_entries(entries)
+
+    def __repr__(self):
+        class_name = type(self).__name__
+        return f"{class_name}(name={self.name!r}, entries={self.entries!r})"
 
     @staticmethod
     def process_entries(entries: list[str]) -> list[MapRanges]:
