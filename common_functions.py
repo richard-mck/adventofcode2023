e6f9f0c469241566ddee4d5bd466780ff906ccab
Richard McKenzie <richard@openoceanrobotics.com>
2023-12-18 15:44:48 -0800

    feat: add common functions to store input data as a grid and print for debugging

--- common_functions.py
+++ common_functions.py
@@ -24,3 +24,25 @@ def parse_data_on_empty_rows(raw_data: list[str]) -> list[list[str]]:
         chunk.append(row)
 
     return result
+
+def transform_data_to_dict_grid(raw_data: list[str]) -> dict:
+    """Given a list of strings, transform this to a dictionary with y/x coordinates as keys"""
+    result = {}
+    for i in range(len(raw_data)):
+        for j in range(len(raw_data[i])):
+            result[i, j] = raw_data[i][j]
+    return result
+
+
+def print_grid(grid: dict, include_row_nums=False):
+    """Given a dictionary grid, print the contents line by line for debugging"""
+    max_y = max(grid.keys())[0]
+    max_x = max(grid.keys())[1]
+    for i in range(0, max_y + 1):
+        for j in range(0, max_x + 1):
+            if include_row_nums and j == 0:
+                print(f"{i}: ", end="")
+            if j == max_x:
+                print(grid[(i, j)])
+            else:
+                print(grid[(i, j)], end="")
